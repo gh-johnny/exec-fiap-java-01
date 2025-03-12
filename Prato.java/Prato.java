3c5735da1034d2758a28c9489498bea57b2aef60
João Romero <jmfurtadoromero@gmail.com>
2025-03-12 19:51:15 -0300

    Create Prato.java

---
+++ Prato.java/Prato.java
@@ -0,0 +1,33 @@
+// Classe Prato
+public class Prato {
+    private String nome;
+    private double preco;
+    private String descricao;
+
+    public Prato(String nome, double preco, String descricao) {
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Prato{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+}
