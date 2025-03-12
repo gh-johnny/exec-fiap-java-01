ce523e0799ff4b2d2f0e86f9fd8ac11ae1ea34a4
João Romero <jmfurtadoromero@gmail.com>
2025-03-12 19:52:15 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,32 @@
+public class Cliente {
+    private String nome;
+    private String telefone;
+    private String endereco;
+
+    public Cliente(String nome, String telefone, String endereco) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", endereco='" + endereco + '\'' +
+                '}';
+    }
+}
